827e5d4c3a90feaffe9ecdd48f7aae5df1c0a592
prathamesh-pc <professionalprathamesh@gmail.com>
2025-11-17 20:46:51 +0530

    Navbar updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ function Navbar() {
                   className="font-semibold font-mono tracking-widest text-2xl 
                     bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary"
                 >
-                  POSGRESTORE
+                  PRODUCT-STORE
                 </span>
               </div>
             </Link>
